c2377d74a2fdc57dcb316cc4d0effa058ffc3b46
Micaela Frank <micaelalfrank@gmail.com>
2022-05-20 10:57:43 -0400

    commit tree a8565c463897c9b2476954731eabeb1454ba4a47

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function focusOnMe(){
+        console.log("Good!");
+    }
+
+    function itsBlurry(){
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={focusOnMe} onBlur={itsBlurry}>
+          Eyes on me
+        </button>
+      );
+}
+
+export default EyesOnMe;
